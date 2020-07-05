d63b700a34b9db03f38d9f00813d7709be330f90
Rishit Chaudhary <rishit.c@live.com>
2020-07-06 04:40:49 +0530

    Changed safetyChecks.h to safetyChecks.c as it contains functions to maintain the safety of dynamic memory usage. Its header had been added to vecfunc.h, in the previous commit.

---
+++ Vector-Pool/src/safetyChecks.c
@@ -0,0 +1,33 @@
+/*
+    This is done to prevent multiple inclusions of the same header file in the project
+*/
+#ifndef STDIO_H
+    #define STDIO_H
+    #include<stdio.h>
+#endif
+
+#ifndef STDLIB_H
+    #define STDLIB_H
+    #include<stdlib.h>
+#endif
+
+#ifndef TYPES_H
+    #define TYPES_H
+    #include "types.h"
+#endif
+
+#ifndef VECFUNC_H
+    #define VECFUNC_H
+    #include "vecfunc.h"
+#endif
+
+
+void checkPtr(VectorElement *vec, char *fileName, int lineNo, char *ptrName) {
+    if(vec == NULL) {
+        printf("Dynamic Memory Allocation Failed!\n");
+        printf("File Name.: %s\n", fileName);
+        printf("Line No.: %4d\n", lineNo);
+        printf("Pointer Name: %s\n", ptrName);
+        exit(EXIT_FAILURE);
+    }
+}
