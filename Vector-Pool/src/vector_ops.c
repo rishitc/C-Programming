02aea3fe382c8cf7b3822e1211d53f8bf364cd85
Rishit Chaudhary <rishit.c@live.com>
2020-07-07 03:15:52 +0530

    Added required header files. Fixed comments. Matched the function definitions to their signatures specified in vecfunc.h. Fixed initial value assignment in createVector(). Typecast additions. Some functions have been modified to take addresse(s) rather than value(s) of the parameter(s).

--- Vector-Pool/src/vector_ops.c
+++ Vector-Pool/src/vector_ops.c
@@ -11,6 +11,16 @@
     #include <stddef.h>
 #endif
 
+#ifndef STDLIB_H
+    #define STDLIB_H
+    #include <stdlib.h>
+#endif
+
+#ifndef MATH_H
+    #define MATH_H
+    #include <math.h>
+#endif
+
 #ifndef TYPES_H
     #define TYPES_H
     #include "types.h"
@@ -24,13 +34,13 @@
 
 /*
     Creating an empty vector of the required size, specified by arg_count.
-    As calloc is being used all variables that have not been assigned a value are
-    automatically set to 0.
 */
-Vector createVector(int arg_count) {  
+Vector createVector(const int arg_count) {  
     Vector newVector;
     newVector.head = calloc(arg_count, sizeof(VectorElement));  // Allocating memory for the vector elements
     checkPtr(newVector.head, __FILE__, __LINE__ - 1, "newVector.head");  // Checking if the calloc memory allocation was successful or not
+
+    newVector.lengthOccupied = 0;
 
     newVector.length = arg_count;  // Storing the number of vector elements in the .length property
 
@@ -42,15 +52,15 @@ Vector createVector(int arg_count) {
     Adds an element at the right end of the vector.
     It returns true if the addition was successful and false if not.
 */
-bool appendElement(Vector vec, double element) {
+bool appendElement(Vector *vec, const double element) {
     // If the vector is full
-    if(vec.lengthOccupied == vec.length) {
+    if(vec -> lengthOccupied == vec -> length) {
         return false;
     }
 
     // The lengthOccupied property will always be 1 greater that the index of the last element in the vector
-    vec.head[vec.lengthOccupied].data = element;
-    ++(vec.lengthOccupied);  // Increment the lenghtOccupied property to reflect the addition of a new element
+    vec -> head[vec -> lengthOccupied].data = element;
+    ++(vec -> lengthOccupied);  // Increment the lenghtOccupied property to reflect the addition of a new element
 
     return true;
 }
@@ -73,9 +83,9 @@ bool destoryVector(Vector *vec) {
 
 
 /*
-    
+    Perform the pooling operation on the vector
 */
-Vector poolVector(Vector vec, int windowSize) {
+Vector poolVector(Vector vec, const int windowSize) {
     /*
         The number of elements in the result after the pooling operation will the
         ceil of the length of the vector and the window size
@@ -83,10 +93,11 @@ Vector poolVector(Vector vec, int windowSize) {
               there will be 3 elements in the result, 2 got from window size 2 and
               1 will be got from the window size 1
     */
-    int numberOfPooledElements = (int)ceil(vec.length / windowSize);
+    int numberOfPooledElements = (int)ceil((double)vec.length / windowSize);
+
     Vector result = createVector(numberOfPooledElements);  // Create the result vector
 
-    for(int elementCount = 0, sum = 0; elementCount < numberOfPooledElements; ++elementCount, sum = 0) {
+    for(int elementCount = 0; elementCount < numberOfPooledElements; ++elementCount) {
         /*
             The START_INDEX is got by multiplying the count for the element in the Pooled result with
             the window size.
@@ -95,8 +106,9 @@ Vector poolVector(Vector vec, int windowSize) {
         const int START_INDEX = elementCount * windowSize;
 
         /*
-            The END_INDEX is got by multiplying the count for the element in the Pooled result with
-            the value which is one greater than the window size.
+            The END_INDEX is got by multiplying the value which is one greater than 
+            the current value of the elementCount with the window size, with one 
+            subtracted from this result.
             E.g.: The END_INDEX gets the values of 1, 3, 5 for a vector of length 5 and window size of 2.
 
             For groups that could be incomplete, as in not having enough elements as expected by the window size,
@@ -104,12 +116,14 @@ Vector poolVector(Vector vec, int windowSize) {
         */
         const int END_INDEX = (elementCount + 1) * windowSize - 1;
 
+        double sum = 0;
         int sumCount = 0;  // To hold the number of elements in the current window sweep
         for(int ctr = START_INDEX; ctr < vec.length && ctr < END_INDEX + 1; ++ctr, ++sumCount) {
             sum += vec.head[ctr].data;
         }
 
-        appendElement(result, (double)sum / sumCount);
+        if(!appendElement(&result, (double)sum / sumCount))
+            break;
     }
 
     return result;
