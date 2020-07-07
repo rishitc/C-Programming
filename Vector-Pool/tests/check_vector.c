145ad0104866b693fca375dce1aa021d638b207f
Rishit Chaudhary <rishit.c@live.com>
2020-07-07 20:55:21 +0530

    Rewrote the test file to reduce the code duplication. I have used the compound literal available in C99. This has helped in the jagged array creation issue, so now the test case data is created in a separate array of a user defined type and the test is common for all and is run for all the test cases separately.

--- Vector-Pool/tests/check_vector.c
+++ Vector-Pool/tests/check_vector.c
@@ -32,8 +32,30 @@
 #define TOLERANCE 5e-7F
 
 
+typedef struct
+{
+    const double *vector;
+    const double *pooledVector;  // Expected answer after pooling operation
+    const int VECTOR_SIZE;
+    const int WINDOW_SIZE;
+}vectorData;
+
+vectorData data[5] = {
+    // Cases where the window size does not fit the vector completely
+    /*0*/ { (double[]){1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}, (double[]){3.50, 7.0}, 7, 6 },
+    /*1*/ { (double[]){1.0, 2.0, 3.0, 4.0, 5.0}, (double[]){1.50, 3.50, 5.0}, 5, 2},
+    /*2*/ { (double[]){4.0, 5.0, 6.0, 3.0, 7.0, 8.0, 10.0, 20.0, 3.4, 5.0}, (double[]){6.142857, 9.466667}, 10, 7},
+
+
+    // Cases where the window size fits the vector completely
+    /*3*/ { (double[]){3.5, 3.45, 7.8, 3.456, 5.67, 10.89, 4.567, 11.98,
+                      101.43, 55.678, 550.432, 0.234, 108.3498, 9.998, 6.7842,
+                      4.567, 29.894, 99.9989, 56.567, 13.3333}, (double[]){4.775200, 36.909000, 135.159600, 40.872040}, 20, 5},
+    /*4*/ { (double[]){1.0, 2.0, 3.0, 4.0}, (double[]){1.50, 3.50}, 4, 2}
+};
+
 /*
-    Run the program for the first example test case:
+    The first example test case:
 	Input:
         7   ---> Size of the vector
         6   ---> Window Size
@@ -47,45 +69,10 @@
 
     Output: 3.50 7.00 
 */
-START_TEST(test_case_one) {
-    extern Vector createVector(int arg_count);
-
-    const int VECTOR_SIZE = 7;
-    const int WINDOW_SIZE = 6;
-
-    double vecElements[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
-    double pooledVecElements[] = {3.50, 7.0};  // Expected answer after pooling operation
-
-    Vector vec = createVector(VECTOR_SIZE);
-
-    // Addition of elements to the vector must return true
-    for(int i = 0; i < VECTOR_SIZE; ++i)
-        ck_assert(appendElement(&vec, vecElements[i]));
-    
-    Vector pooledVec = poolVector(vec, WINDOW_SIZE);
-
-    // Check if the pooledVec contains the correct elements
-    int numberOfPooledElements = (int)ceil((double)VECTOR_SIZE / WINDOW_SIZE);
-    for(int  i = 0; i < numberOfPooledElements; ++i) {
-        ck_assert_double_eq_tol(pooledVec.head[i].data, pooledVecElements[i], TOLERANCE);
-    }
-
-    // Destroying the vector must return true
-    ck_assert(destoryVector(&pooledVec));
-    ck_assert(destoryVector(&vec));
-
-    ck_assert_ptr_eq(pooledVec.head, NULL);
-    ck_assert_int_eq(pooledVec.length, 0);
-    ck_assert_int_eq(pooledVec.lengthOccupied, 0);
-
-    ck_assert_ptr_eq(vec.head, NULL);
-    ck_assert_int_eq(vec.length, 0);
-    ck_assert_int_eq(vec.lengthOccupied, 0);
-} END_TEST
 
 
 /*
-    Run the program for the second example test case:
+    The second example test case:
 	Input:         
         5  ---> Size of the vector
         2  ---> Window Size
@@ -97,45 +84,10 @@ START_TEST(test_case_one) {
 
     Output: 1.50 3.50 5.00 
 */
-START_TEST(test_case_two) {
-    extern Vector createVector(int arg_count);
-
-    const int VECTOR_SIZE = 5;
-    const int WINDOW_SIZE = 2;
-
-    double vecElements[] = {1.0, 2.0, 3.0, 4.0, 5.0};
-    double pooledVecElements[] = {1.50, 3.50, 5.0};  // Expected answer after pooling operation
-
-    Vector vec = createVector(VECTOR_SIZE);
-
-    // Addition of elements to the vector must return true
-    for(int i = 0; i < VECTOR_SIZE; ++i)
-        ck_assert(appendElement(&vec, vecElements[i]));
-    
-    Vector pooledVec = poolVector(vec, WINDOW_SIZE);
-
-    // Check if the pooledVec contains the correct elements
-    int numberOfPooledElements = (int)ceil((double)VECTOR_SIZE / WINDOW_SIZE);
-    for(int  i = 0; i < numberOfPooledElements; ++i) {
-        ck_assert_double_eq_tol(pooledVec.head[i].data, pooledVecElements[i], TOLERANCE);
-    }
-
-    // Destroying the vector must return true
-    ck_assert(destoryVector(&pooledVec));
-    ck_assert(destoryVector(&vec));
-
-    ck_assert_ptr_eq(pooledVec.head, NULL);
-    ck_assert_int_eq(pooledVec.length, 0);
-    ck_assert_int_eq(pooledVec.lengthOccupied, 0);
-
-    ck_assert_ptr_eq(vec.head, NULL);
-    ck_assert_int_eq(vec.length, 0);
-    ck_assert_int_eq(vec.lengthOccupied, 0);
-} END_TEST
 
 
 /*
-    Run the program for the third example test case:
+    The third example test case:
 	Input:         
         4  ---> Size of the vector
         2  ---> Window Size
@@ -146,45 +98,10 @@ START_TEST(test_case_two) {
 
     Output: 1.50 3.50
 */
-START_TEST(test_case_three) {
-    extern Vector createVector(int arg_count);
-
-    const int VECTOR_SIZE = 4;
-    const int WINDOW_SIZE = 2;
-
-    double vecElements[] = {1.0, 2.0, 3.0, 4.0};
-    double pooledVecElements[] = {1.50, 3.50};  // Expected answer after pooling operation
-
-    Vector vec = createVector(VECTOR_SIZE);
-
-    // Addition of elements to the vector must return true
-    for(int i = 0; i < VECTOR_SIZE; ++i)
-        ck_assert(appendElement(&vec, vecElements[i]));
-    
-    Vector pooledVec = poolVector(vec, WINDOW_SIZE);
-
-    // Check if the pooledVec contains the correct elements
-    int numberOfPooledElements = (int)ceil((double)VECTOR_SIZE / WINDOW_SIZE);
-    for(int  i = 0; i < numberOfPooledElements; ++i) {
-        ck_assert_double_eq_tol(pooledVec.head[i].data, pooledVecElements[i], TOLERANCE);
-    }
-
-    // Destroying the vector must return true
-    ck_assert(destoryVector(&pooledVec));
-    ck_assert(destoryVector(&vec));
-
-    ck_assert_ptr_eq(pooledVec.head, NULL);
-    ck_assert_int_eq(pooledVec.length, 0);
-    ck_assert_int_eq(pooledVec.lengthOccupied, 0);
-
-    ck_assert_ptr_eq(vec.head, NULL);
-    ck_assert_int_eq(vec.length, 0);
-    ck_assert_int_eq(vec.lengthOccupied, 0);
-} END_TEST
 
 
 /*
-    Run the program for the fourth example test case:
+    The fourth example test case:
 	Input:         
         10  ---> Size of the vector
         7   ---> Window Size
@@ -201,45 +118,10 @@ START_TEST(test_case_three) {
 
     Output: 6.142857 9.466667
 */
-START_TEST(test_case_four) {
-    extern Vector createVector(int arg_count);
-
-    const int VECTOR_SIZE = 10;
-    const int WINDOW_SIZE = 7;
-
-    double vecElements[] = {4.0, 5.0, 6.0, 3.0, 7.0, 8.0, 10.0, 20.0, 3.4, 5.0};
-    double pooledVecElements[] = {6.142857, 9.466667};  // Expected answer after pooling operation
-
-    Vector vec = createVector(VECTOR_SIZE);
-
-    // Addition of elements to the vector must return true
-    for(int i = 0; i < VECTOR_SIZE; ++i)
-        ck_assert(appendElement(&vec, vecElements[i]));
-    
-    Vector pooledVec = poolVector(vec, WINDOW_SIZE);
-
-    // Check if the pooledVec contains the correct elements
-    int numberOfPooledElements = (int)ceil((double)VECTOR_SIZE / WINDOW_SIZE);
-    for(int  i = 0; i < numberOfPooledElements; ++i) {
-        ck_assert_double_eq_tol(pooledVec.head[i].data, pooledVecElements[i], TOLERANCE);
-    }
-
-    // Destroying the vector must return true
-    ck_assert(destoryVector(&pooledVec));
-    ck_assert(destoryVector(&vec));
-
-    ck_assert_ptr_eq(pooledVec.head, NULL);
-    ck_assert_int_eq(pooledVec.length, 0);
-    ck_assert_int_eq(pooledVec.lengthOccupied, 0);
-
-    ck_assert_ptr_eq(vec.head, NULL);
-    ck_assert_int_eq(vec.length, 0);
-    ck_assert_int_eq(vec.lengthOccupied, 0);
-} END_TEST
 
 
 /*
-    Run the program for the fifth example test case:
+    The fifth example test case:
 	Input:         
         20  ---> Size of the vector
         5   ---> Window Size
@@ -266,16 +148,14 @@ START_TEST(test_case_four) {
 
     Output: 4.775200 36.909000 135.159600 40.872040
 */
-START_TEST(test_case_five) {
+START_TEST(complete_vector_life_cycle_test) {
     extern Vector createVector(int arg_count);
 
-    const int VECTOR_SIZE = 20;
-    const int WINDOW_SIZE = 5;
+    const int VECTOR_SIZE = data[_i].VECTOR_SIZE;
+    const int WINDOW_SIZE = data[_i].WINDOW_SIZE;
 
-    double vecElements[] = {3.5, 3.45, 7.8, 3.456, 5.67, 10.89, 4.567, 11.98,
-                            101.43, 55.678, 550.432, 0.234, 108.3498, 9.998, 6.7842,
-                            4.567, 29.894, 99.9989, 56.567, 13.3333};
-    double pooledVecElements[] = {4.775200, 36.909000, 135.159600, 40.872040};  // Expected answer after pooling operation
+    const double *vecElements = data[_i].vector;
+    const double *pooledVecElements = data[_i].pooledVector;  // Expected answer after pooling operation
 
     Vector vec = createVector(VECTOR_SIZE);
 
@@ -313,12 +193,8 @@ Suite *vector_suite(void) {
     TCase *tc_fit = tcase_create("Window size fits the vector size");  // Creating a test case to add to the suite
     TCase *tc_no_fit = tcase_create("Window size does not fit the vector size");  // Creating a test case to add to the suite
 
-    tcase_add_test(tc_fit, test_case_three);
-    tcase_add_test(tc_fit, test_case_five);
-
-    tcase_add_test(tc_no_fit, test_case_one);
-    tcase_add_test(tc_no_fit, test_case_two);
-    tcase_add_test(tc_no_fit, test_case_four);
+    tcase_add_loop_test(tc_no_fit, complete_vector_life_cycle_test, 0, 3);
+    tcase_add_loop_test(tc_fit, complete_vector_life_cycle_test, 3, 5);
 
     suite_add_tcase(s, tc_fit);
     suite_add_tcase(s, tc_no_fit);
@@ -330,7 +206,7 @@ Suite *vector_suite(void) {
 int main() {
     int no_failed = 0;                   
     Suite *s;                            
-    SRunner *runner;                     
+    SRunner *runner;           
 
     s = vector_suite();                   
     runner = srunner_create(s);          
